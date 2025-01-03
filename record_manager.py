250665ddc57be2ad26f6da5762e6448018340412
Agustín <agustincalpedm@gmail.com>
2025-01-03 01:07:11 -0300

    exe

--- record_manager.py
+++ record_manager.py
@@ -1,13 +1,27 @@
 import os
+import sys
+import shutil
+
 
 class RecordManager:
 
     def __init__(self):
-        self.path = "records.csv"
+        base_dir = os.path.join(os.getenv('APPDATA'), "Minesweeper")
+        os.makedirs(base_dir, exist_ok=True)
+
+        self.path = os.path.join(base_dir, "records.csv")
+
+        if not os.path.exists(self.path):
+            if getattr(sys, 'frozen', False):  # Si es un ejecutable
+                original_csv = os.path.join(sys._MEIPASS, "records.csv")
+            else:
+                original_csv = os.path.join(
+                    os.path.dirname(__file__), "records.csv")
+            shutil.copyfile(original_csv, self.path)
 
     def get_record_in_difficulty(self, difficulty: str):
         """Return the record in seconds of the dificculty passed"""
-        if os.path.exists(self.path):    
+        if os.path.exists(self.path):
             with open(self.path, "r", encoding="utf-8") as file:
                 record = 0
                 for line in file:
@@ -37,5 +51,5 @@ class RecordManager:
                 if not replace:
                     file.write(difficulty + ',' + record + '\n')
         else:
-            with open(self.path,"w", encoding="utf-8") as file:
+            with open(self.path, "w", encoding="utf-8") as file:
                 file.write(difficulty + ',' + record + '\n')
